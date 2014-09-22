296f073ee830047f35ff9e7a09e1f8f5d1cb7059
Xinhao Yuan <xinhaoyuan@gmail.com>
2014-09-22 14:17:32 -0400

    .

--- generate_slides.py
+++ generate_slides.py
@@ -4,6 +4,7 @@
 
 import sys, os, subprocess
 import markdown
+import re
 
 def main():
   # cur_process = subprocess.Popen(
@@ -11,6 +12,7 @@ def main():
   #   stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell = True)
   slide_buffer = ''
   in_content = False
+  options = {}
   for line in sys.stdin:
     if is_title_line(line):
       if in_content:
@@ -21,16 +23,27 @@ def main():
         #   stdin = subprocess.PIPE, stdout = subprocess.PIPE, shell = True)
         slide_buffer = ''
       else:
+        # XXX write options
+        sys.stdout.write('<div id="slides"')
+        for i in options:
+          if options[i].startswith('"'):
+            sys.stdout.write(' %s=%s' % (i, options[i]))
+          else:
+            sys.stdout.write(' %s="%s"' % (i, options[i]))
+        sys.stdout.write('>\n')
         in_content = True
     
     if in_content:
       slide_buffer += line
     else:
-      # XXX process some options?
+      m = re.match('\s*([a-zA-Z_-]+)\s*=(.*)', line)
+      if m:
+        options[m.group(1)] = m.group(2).strip()
       pass
   if in_content and len(slide_buffer) > 0:
     write_slide(markdown.markdown(slide_buffer))
     # write_slide(cur_process.communicate(slide_buffer)[0])
+  sys.stdout.write('</div>\n')
     
 def is_title_line(line):
     sline = line.strip()
